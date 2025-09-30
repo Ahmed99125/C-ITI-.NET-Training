5f11a02e6e61bdf5c5c57648c7743f1427cc8be4
Ahmed99125 <at99125@gmail.com>
2025-09-30 20:28:30 +0300

    mvc task 1 & 2

---
+++ WebApplication1/Data/iti_project_dbContext.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using System.Reflection.Emit;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<CourseStudent> CourseStudents { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<CourseStudent>()
+                .HasKey(cs => new { cs.CrsId, cs.StdId });
+
+            modelBuilder.Entity<CourseStudent>()
+                .HasOne(cs => cs.Course)
+                .WithMany(c => c.CourseStudents)
+                .HasForeignKey(cs => cs.CrsId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<CourseStudent>()
+                .HasOne(cs => cs.Student)
+                .WithMany(s => s.CourseStudents)
+                .HasForeignKey(cs => cs.StdId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            modelBuilder.Entity<Course>()
+                .HasOne(c => c.Department)
+                .WithMany(d => d.Courses)
+                .HasForeignKey(c => c.DeptId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Student>()
+                .HasOne(s => s.Department)
+                .WithMany(d => d.Students)
+                .HasForeignKey(s => s.DeptId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Instructor>()
+                .HasOne(i => i.Department)
+                .WithMany(d => d.Instructors)
+                .HasForeignKey(i => i.DeptId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Instructor>()
+                .HasOne(i => i.Course)
+                .WithMany(c => c.Instructors)
+                .HasForeignKey(i => i.CrsId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+
+    }
+}
